ae0bfc6573c0ad377faa02d9468f4b78e14b4ff3
micaelacolpi <micaelacolpi@yahoo.com>
2021-04-08 11:00:33 -0600

    getting caught up

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { Home } from "../Home"
+import {AnimalCard} from "./animal/AnimalCard"
+import {EmployeeCard} from "./employee/EmployeeCard"
+import {CustomerCard} from "./customer/CustomerCard"
+import {LocationCard} from "./location/LocationCard"
+import { AnimalList } from "./animal/AnimalList"
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            {/* Render the location list when http://localhost:3000/ */}
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <Route path="/animals">
+              <AnimalList />
+            </Route>
+            <Route path="/locations">
+                <LocationCard />
+            </Route>
+            <Route path="/employees">
+                <EmployeeCard />
+            </Route>
+            <Route path="/customers">
+                <CustomerCard />
+            </Route>
+        </>
+    )
+}
